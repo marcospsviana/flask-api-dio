9775f4b0501776b80225e60a41033c801239fe34
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-10-03 12:25:24 -0300

    retornado mod hora 10:23"

--- tests/test_users.py
+++ tests/test_users.py
@@ -8,17 +8,8 @@ def test_login_fail_username(client, db):
 
 def test_login_fail_password(client, db):
     payload = json.dumps({"username": "devpro", "password": "null"})
-<<<<<<< HEAD
     assert client.post("/authenticate", data=payload).status_code == 400
 
-=======
-    assert client.post("/authenticate", data=payload).status_code == 403
-
-
-def test_user_create(client, db):
-    payload = json.dumps({"username": "teste", "passord": "teste"})
-    assert client.post("/create-user", data=payload)
->>>>>>> 94f1ea30bd90da17b14242862bf049a0b5ab9f9e
 
 def test_user_create(client, db):
     payload = json.dumps({"username": "teste", "passord": "teste"})
@@ -35,14 +26,8 @@ def test_user_login_failed(client, db):
 
 def test_user_login_successful(client, db):
     headers = {"content-type": "application/json"}
-<<<<<<< HEAD
     payload = json.dumps({"username": "devtest", "password": "devnull"})
     assert (
         client.post("/authenticate", data=payload).status_code == 202
-=======
-    payload = json.dumps({"username": "devpro", "password": "devnull"})
-    assert (
-        client.post("/authenticate", headers=headers, data=payload).status_code == 202
->>>>>>> 94f1ea30bd90da17b14242862bf049a0b5ab9f9e
     )
 
